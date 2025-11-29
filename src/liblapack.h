eee640cd0068f2f6a03a0eb242e0901a15ec9d4c
gconant0 <54152176+gconant0@users.noreply.github.com>
2025-11-29 11:17:42 -0500

    Add files via upload

--- src/liblapack.h
+++ src/liblapack.h
@@ -8,6 +8,10 @@ extern "C" int dgetri_(long int *n, double *a, long int *lda,
 //Computes the LU factorization of a matrix
 extern "C" int dgetrf_ (long int *m, long int *n, double *a, long int *lda, 
 			long int *ipiv, long int *info);
+//extern "C" int dgetrf_ (int *m, int *n, double *a,  int *lda,
+  //                     int *ipiv,  int *info);
+
+
 
 //Computes the eigen values and vectors of a matrix 
 extern "C" int dgeev_(char *jobvl, char *jobvr, long int *n, 
@@ -25,5 +29,23 @@ extern "C" int dgemm_ (char *transa, char *transb, long int *m, long int *n,
 		       long int *transa_len, long int *transb_len);
 
 
+//extern "C" int dgemm_ (char *transa, char *transb, long int *m, long int *n,
+//                       long int *k, double *alpha, double *a,
+//                       long int *lda, double *b, long int *ldb,
+//                       double *beta, double *c__, long int *ldc);
 
 
+
+//Solves over or underdetermined system
+extern "C" int dgels_(char *trans, int *m, int *n, int *nrhs, double *a, int* lda, double *b, int* ldb, double *work, int* lwork, int* info);
+
+
+//LQ factorization of underdetermined system
+extern "C" int dgelqf_(int* m, int* n, double* a, int* lda, double* tau, double* work, int* lwork, int* info);
+
+
+extern "C" void dgesdd_(char *JOBZ,  int *M, int *N, double *A, int *LDA,double *S,          double *U, int *LDU,   double *VT, int *LDVT,  double *WORK, int *LWORK, int *IWORK,            int *INFO);
+
+
+extern "C" void dgesvd_(char *JOBU, char *JOBVT, int *M, int *N, double *A, int *LDA,  double *S,
+                        double *U, int *LDU,double *VT, int *LDVT,  double *WORK, int *LWORK,   int *INFO);
