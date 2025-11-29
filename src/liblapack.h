ea48277182b48fe28275b4a5fc3b855d93a08991
gconant0 <54152176+gconant0@users.noreply.github.com>
2025-11-29 11:25:18 -0500

    Add files via upload

--- src/liblapack.h
+++ src/liblapack.h
@@ -1,32 +1,32 @@
 //c++ include file for the library lbprsh_lapack
 //Computes the inverse of a matrix--requires LU factored matrix
 //from dgetrf
-extern "C" int dgetri_(long int *n, double *a, long int *lda, 
-		       long int *ipiv, double *work, long int *lwork, 
-		       long int *info);
+extern "C" int dgetri_( int *n, double *a, int *lda,
+		        int *ipiv, double *work, int *lwork,
+		       int *info);
 
 //Computes the LU factorization of a matrix
-extern "C" int dgetrf_ (long int *m, long int *n, double *a, long int *lda, 
-			long int *ipiv, long int *info);
+extern "C" int dgetrf_ (int *m, int *n, double *a,  int *lda,
+			int *ipiv, int *info);
 //extern "C" int dgetrf_ (int *m, int *n, double *a,  int *lda,
   //                     int *ipiv,  int *info);
 
 
 
 //Computes the eigen values and vectors of a matrix 
-extern "C" int dgeev_(char *jobvl, char *jobvr, long int *n, 
-		  double *a, long int *lda, double *wr, 
-		  double *wi, double *vl, long int *ldvl, 
-		  double *vr, long int *ldvr, double *work, 
-		  long int *lwork, long int *info, long int *jobvl_len, 
-		  long int *jobvr_len);
+extern "C" int dgeev_(char *jobvl, char *jobvr,  int *n,
+		  double *a, int *lda, double *wr,
+		  double *wi, double *vl, int *ldvl,
+		  double *vr,  int *ldvr, double *work,
+		  int *lwork, int *info, int *jobvl_len,
+		  int *jobvr_len);
 
 //Performs a matrix/matrix multiplication
-extern "C" int dgemm_ (char *transa, char *transb, long int *m, long int *n, 
-		       long int *k, double *alpha, double *a, 
-		       long int *lda, double *b, long int *ldb, 
-		       double *beta, double *c__, long int *ldc, 
-		       long int *transa_len, long int *transb_len);
+extern "C" int dgemm_ (char *transa, char *transb, int *m, int *n,
+		        int *k, double *alpha, double *a,
+		        int *lda, double *b,  int *ldb,
+		       double *beta, double *c__,  int *ldc,
+		        int *transa_len, int *transb_len);
 
 
 //extern "C" int dgemm_ (char *transa, char *transb, long int *m, long int *n,
